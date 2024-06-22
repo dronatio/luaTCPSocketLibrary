d08003c1d65dc006cc77819c747977c557fc60ed
dronatio <pipocapaloca@gmail.com>
2024-06-22 13:40:22 -0300

    source files

---
+++ tcp_client.c
@@ -0,0 +1,179 @@
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <string.h>
+#include <process.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+// Lua state and callback references
+static lua_State *gL = NULL;
+static int gReceiveCallbackRef = LUA_NOREF;
+static int gDisconnectCallbackRef = LUA_NOREF;
+
+// Initialize Winsock
+static int l_init(lua_State *L) {
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        lua_pushstring(L, "WSAStartup failed");
+        lua_error(L);
+    }
+    lua_pushboolean(L, 1);
+    return 1;
+}
+
+// Create a TCP socket
+static int l_create_socket(lua_State *L) {
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sock == INVALID_SOCKET) {
+        lua_pushstring(L, "Socket creation failed");
+        lua_error(L);
+    }
+    lua_pushlightuserdata(L, (void *)sock);
+    return 1;
+}
+
+// Connect to a server
+static int l_connect(lua_State *L) {
+    SOCKET sock = (SOCKET)lua_touserdata(L, 1);
+    const char *ip = luaL_checkstring(L, 2);
+    int port = luaL_checkinteger(L, 3);
+
+    struct sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = inet_addr(ip);
+
+    int iResult = connect(sock, (struct sockaddr *)&server, sizeof(server));
+    if (iResult == SOCKET_ERROR) {
+        lua_pushstring(L, "Connection failed");
+        lua_error(L);
+    }
+    lua_pushboolean(L, 1);
+    return 1;
+}
+
+// Set the Lua callback function for receiving data
+static int l_set_receive_callback(lua_State *L) {
+    if (lua_isfunction(L, 1)) {
+        lua_pushvalue(L, 1);
+        gReceiveCallbackRef = luaL_ref(L, LUA_REGISTRYINDEX);
+        gL = L;
+    }
+    return 0;
+}
+
+// Set the Lua callback function for disconnection
+static int l_set_disconnect_callback(lua_State *L) {
+    if (lua_isfunction(L, 1)) {
+        lua_pushvalue(L, 1);
+        gDisconnectCallbackRef = luaL_ref(L, LUA_REGISTRYINDEX);
+        gL = L;
+    }
+    return 0;
+}
+
+// Internal function to invoke the Lua callback
+static void invoke_lua_callback(int callbackRef, const char *data) {
+    if (gL != NULL && callbackRef != LUA_NOREF) {
+        lua_rawgeti(gL, LUA_REGISTRYINDEX, callbackRef);
+        lua_pushstring(gL, data);
+        if (lua_pcall(gL, 1, 0, 0) != LUA_OK) {
+            const char *error = lua_tostring(gL, -1);
+            printf("Error calling Lua callback: %s\n", error);
+            lua_pop(gL, 1);
+        }
+    }
+}
+
+// Asynchronous receive function
+static void receive_data(void *param) {
+    SOCKET sock = (SOCKET)param;
+    char buffer[1024];
+    int iResult;
+
+    do {
+        iResult = recv(sock, buffer, sizeof(buffer), 0);
+        if (iResult > 0) {
+            buffer[iResult] = '\0';
+            invoke_lua_callback(gReceiveCallbackRef, buffer);
+        } else if (iResult == 0) {
+            printf("Connection closed\n");
+        } else {
+            int resulterrorgeted = WSAGetLastError();
+            if (resulterrorgeted == 10054) {
+                printf("Connection exited\n");
+                iResult = 0;
+            }
+        }
+    } while (iResult > 0);
+
+    // Invoke the disconnect callback when the connection is closed
+    invoke_lua_callback(gDisconnectCallbackRef, "Client disconnected");
+
+    closesocket(sock);
+}
+
+// Receive data asynchronously
+static int l_receive_async(lua_State *L) {
+    SOCKET sock = (SOCKET)lua_touserdata(L, 1);
+
+    // Start a new thread to receive data asynchronously
+    _beginthread(receive_data, 0, (void *)sock);
+
+    lua_pushboolean(L, 1);
+    return 1;
+}
+
+// Send data
+static int l_send(lua_State *L) {
+    SOCKET sock = (SOCKET)lua_touserdata(L, 1);
+    const char *data = luaL_checkstring(L, 2);
+    int len = strlen(data);
+
+    int iResult = send(sock, data, len, 0);
+    if (iResult == SOCKET_ERROR) {
+        lua_pushstring(L, "Send failed");
+        lua_error(L);
+    }
+    lua_pushboolean(L, 1);
+    return 1;
+}
+
+// Close the socket
+static int l_close_socket(lua_State *L) {
+    SOCKET sock = (SOCKET)lua_touserdata(L, 1);
+    closesocket(sock);
+    lua_pushboolean(L, 1);
+    return 1;
+}
+
+// Clean up Winsock
+static int l_cleanup(lua_State *L) {
+    WSACleanup();
+    lua_pushboolean(L, 1);
+    return 1;
+}
+
+// Register the functions
+static const struct luaL_Reg mylib[] = {
+    {"init", l_init},
+    {"create_socket", l_create_socket},
+    {"connect", l_connect},
+    {"send", l_send},
+    {"receive_async", l_receive_async},
+    {"close_socket", l_close_socket},
+    {"cleanup", l_cleanup},
+    {"set_receive_callback", l_set_receive_callback},
+    {"set_disconnect_callback", l_set_disconnect_callback},
+    {NULL, NULL}
+};
+
+int luaopen_tcp_client(lua_State *L) {
+    luaL_newlib(L, mylib);
+    return 1;
+}
